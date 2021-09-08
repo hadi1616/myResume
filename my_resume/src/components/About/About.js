fb4dbac66453c7c875025702e3f0d53057fa3b38
Hadi Moallem <hadimoallem1999@gmail.com>
2021-09-09 00:33:28 +0300

    homePush

--- my_resume/src/components/About/About.js
+++ my_resume/src/components/About/About.js
@@ -1,22 +1,34 @@
 import Navbar from '../NavBar/NavBar'
 import '../About/About.css'
 import personalImg from '../../image/linkedin pic.jpeg'
-
+import { Button } from 'reactstrap';
+import jsPDF from 'jspdf'
+import myResume from '../Resume/Hadi_Moallem_CV.pdf'
 
 {/* <Navbar/> */ }
 
-function About() {
-        var body = document.body,
-                html = document.documentElement;
+// var body = document.body,
+//         html = document.documentElement;
 
-        var height = Math.max(body.scrollHeight, body.offsetHeight,
-                html.clientHeight, html.scrollHeight, html.offsetHeight);
+// var height = Math.max(body.scrollHeight, body.offsetHeight,
+//         html.clientHeight, html.scrollHeight, html.offsetHeight);
+function About() {
+
+        function pdfGenerate() {
+                var doc = new jsPDF('landscape', 'px', 'a4', 'false');
+                doc.addPage(myResume, 'PDF', 65, 20, 500, 400)
+                doc.save('resume.pdf')
+        }
+
         return (
                 <div className={'container'}>
                         <img className={'personalPic'} src={personalImg} />
                         <div className={'aboutMe'}>
                                 <p>Practical engineer with high motivation and passion for succeeding and  progressing, excellent <br />communication skills with team members , high responsibility.<br /> <div className={'lookingFor'}>Looking for entry level positions in the Front-end , Back-end , Full stack developer</div></p>
                         </div>
+                        {/* <button><a href="../Resume/Hadi_Moallem_CV.pdf" download> Download CV</a></button> */}
+
+                        <Button onClick={pdfGenerate} className={'downloadButton'}> Download resume</Button>
                 </div>
         )
 }
