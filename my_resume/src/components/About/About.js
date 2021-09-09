11e4ea49100d34adb01633e7528ac9e1e6d7caf1
Moallem <hadix.moallem@intel.com>
2021-09-09 13:05:48 +0300

    pushing from work

--- my_resume/src/components/About/About.js
+++ my_resume/src/components/About/About.js
@@ -1,34 +1,67 @@
 import Navbar from '../NavBar/NavBar'
 import '../About/About.css'
-import personalImg from '../../image/linkedin pic.jpeg'
+import personalImg from '../../image/linkedin pic3.jpeg'
 import { Button } from 'reactstrap';
 import jsPDF from 'jspdf'
 import myResume from '../Resume/Hadi_Moallem_CV.pdf'
 
-{/* <Navbar/> */ }
 
-// var body = document.body,
-//         html = document.documentElement;
+import Tab from '@material-ui/core/Tab';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import { Avatar } from '@material-ui/core';
+import Slide from "react-reveal";
 
-// var height = Math.max(body.scrollHeight, body.offsetHeight,
-//         html.clientHeight, html.scrollHeight, html.offsetHeight);
+
 function About() {
 
         function pdfGenerate() {
                 var doc = new jsPDF('landscape', 'px', 'a4', 'false');
                 doc.addPage(myResume, 'PDF', 65, 20, 500, 400)
-                doc.save('resume.pdf')
+                doc.save('Hadi_Moallem_Resume.pdf')
         }
 
-        return (
-                <div className={'container'}>
-                        <img className={'personalPic'} src={personalImg} />
-                        <div className={'aboutMe'}>
-                                <p>Practical engineer with high motivation and passion for succeeding and  progressing, excellent <br />communication skills with team members , high responsibility.<br /> <div className={'lookingFor'}>Looking for entry level positions in the Front-end , Back-end , Full stack developer</div></p>
-                        </div>
-                        {/* <button><a href="../Resume/Hadi_Moallem_CV.pdf" download> Download CV</a></button> */}
 
-                        <Button onClick={pdfGenerate} className={'downloadButton'}> Download resume</Button>
+        const useStyles = makeStyles((theme) => ({
+                root: {
+                        display: 'flex',
+                        '& > *': {
+                                margin: theme.spacing(1),
+                        },
+                },
+
+                large: {
+                        width: theme.spacing(20),
+                        height: theme.spacing(20),
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                },
+        }));
+
+
+        const classes = useStyles();
+
+        return (
+
+                <div className={'container'}>
+
+                        {/* <img className={'personalPic'} src={personalImg} /> */}
+                        <Slide top duration={1100}>
+                                <div className={'AvatarDiv'}>
+                                        <Avatar alt="Remy Sharp" src={personalImg} className={classes.large} />
+                                </div>
+                        </Slide>
+                        <Slide top duration={1100}>
+                                <div className={'aboutMe'}>
+                                        <p>Practical engineer with high motivation and passion for succeeding and  progressing, excellent <br />communication skills with team members , high responsibility.<br /> <div className={'lookingFor'}>Looking for entry level positions in the Front-end , Back-end , Full stack developer</div></p>
+                                </div>
+                                {/* <button><a href="../Resume/Hadi_Moallem_CV.pdf" download> Download CV</a></button> */}
+
+                        </Slide>
+                        <Slide top duration={1100}>
+                                <Button onClick={pdfGenerate} className={'downloadButton'}> Download resume</Button>
+                        </Slide>
+
                 </div>
         )
 }
--- my_resume/src/components/NavBar/NavBar.js
+++ my_resume/src/components/NavBar/NavBar.js
@@ -1,33 +1,17 @@
 import React from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
-// import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-// import Typography from '@material-ui/core/Typography';
-// import { Link } from 'react-router-dom';
-// import NavBar from "../NavBar/NavBar.css"
 import { useHistory } from "react-router-dom"; //i can make the nav bar sticky to but it on the home component and stay scrolling down to the other components
 
 
-  // const StyledTabs = withStyles({
-  //   indicator: {
-  //     display: 'flex',
-  //     justifyContent: 'center',
-  //     backgroundColor: 'transparent',
-  //     '& > span': {
-  //       maxWidth: 55,
-  //       width: '100%',
-  //       backgroundColor: '#fa9d39',
-  //     },
-  //   },
-  // })((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
   
   const StyledTab = withStyles((theme) => ({
     root: {
       textTransform: 'none',
       color: '#fff',
       fontWeight: theme.typography.fontWeightRegular,
-      fontSize: theme.typography.pxToRem(16),
-      marginRight: theme.spacing(0),
+      fontSize: theme.typography.pxToRem(14),
+      marginRight: theme.spacing(1),
       '&:focus': {
         opacity: 1,
       },
@@ -45,7 +29,7 @@ import { useHistory } from "react-router-dom"; //i can make the nav bar sticky t
       backgroundColor: '#343d40',
       display: 'flex',
       justifyContent: 'center',
-      padding: theme.spacing(0.5),
+      padding: theme.spacing(0),
       position:'sticky',
     },
   }));
@@ -53,24 +37,14 @@ import { useHistory } from "react-router-dom"; //i can make the nav bar sticky t
   export default function CustomizedTabs() {
     const history = useHistory();
     const classes = useStyles();
-    // const [value, setValue] = React.useState(0);
-  
-    // const handleChange = (event, newValue) => {
-    //   setValue(newValue);
-      
-    // };
-  
+
     return (
       <div className={classes.root}>
         <div className={classes.demo2}>
-          {/* <StyledTabs value={value} onChange={handleChange} aria-label="styled tabs example"> */}
             <StyledTab label="Home" className={classes.indicator} onClick={(e) => { history.push("/"); }}/>
             <StyledTab label="About" className={classes.indicator} onClick={(e) => { history.push("/about"); }}/>
-            {/* <StyledTab label="Resume" className={classes.indicator} onClick={(e) => { history.push("/resume"); }}/> */}
             <StyledTab label="Works" className={classes.indicator} onClick={(e) => { history.push("/works"); }}/>
             <StyledTab label="Contact" className={classes.deindicatormo3} onClick={(e) => { history.push("/contact"); }}/>
-          {/* </StyledTabs> */}
-          {/* <Typography className={classes.padding} /> */}
         </div>
       </div>
       
